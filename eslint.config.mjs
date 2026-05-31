7637eece976df3b01133638c5b65a992a51c78a8
Andrei <jep_95@mail.ru>
2026-05-31 13:04:56 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,ts,jsx,tsx}'],
+    plugins: { js, 'react-hooks': reactHooks, 'react-refresh': reactRefresh },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+    eslintConfigPrettier,
+    rules: {
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'object-curly-spacing': ['error', 'never'],
+      'array-bracket-spacing': ['error', 'never'],
+      'no-multi-spaces': 'error',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      eqeqeq: ['error', 'always'],
+      'no-unused-vars': 'warn',
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': 'warn',
+    },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
